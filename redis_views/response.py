9136eff99a033c1d76badd5394e654b96e401ca6
Ahmed Nassar <ahmed@ahmed.mdr.wiredrive.com>
2015-10-20 15:21:10 -0700

    Added socket timeout for the client connection to raise an error if the URL is invalid. Also, added error logging if the template key was not found in the DB.

--- redis_views/response.py
+++ redis_views/response.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.template.response import TemplateResponse
 from django.template.base import Template
 from django.conf import settings
@@ -5,12 +7,22 @@ from django.http import Http404
 
 import redis
 
+logger = logging.getLogger(__name__)
+
 
 class RedisTemplateResponse(TemplateResponse):
 
     def __init__(self, *args, **kwargs):
         super(RedisTemplateResponse, self).__init__(*args, **kwargs)
-        self.connection = redis.StrictRedis.from_url(settings.REDIS_URL)
+        socket_timeout = 2
+        if hasattr(settings, 'REDIS_CLIENT_SOCKET_TIMEOUT'):
+            socket_timeout = settings.REDIS_CLIENT_SOCKET_TIMEOUT
+        self.connection = redis.StrictRedis.from_url(settings.REDIS_URL,
+                                                     socket_timeout=socket_timeout)
+
+        # ping the server to make sure we have a valid connection. This will raise a
+        # ConnectionError if the redis URL is invalid.
+        self.connection.ping()
 
     def resolve_template(self, template):
         template = template[0]
@@ -19,6 +31,10 @@ class RedisTemplateResponse(TemplateResponse):
 
         template_body = self.connection.get(template)
         if not template_body:
+            # log an error message if the template key was not found in the DB
+            msg = 'Reids Template: Could not find the template key {}'.format(template)
+            logger.error(msg)
+
             raise Http404()
 
         return Template(template_body)
--- tests/test_response.py
+++ tests/test_response.py
@@ -1,7 +1,9 @@
 import pytest
-from mock import Mock
+import mock
+import redis
+
 from django.http import Http404
-from redis_views.response import RedisTemplateResponse
+from redis_views.response import RedisTemplateResponse, logger
 
 
 class TestRedisTemplateResponse:
@@ -9,17 +11,20 @@ class TestRedisTemplateResponse:
     @pytest.fixture
     def response(self):
         response = RedisTemplateResponse(request=None, template='')
-        response.connection = Mock()
+        response.connection = mock.Mock()
         return response
 
     def test_it_resolves_template_to_named_version(self, response):
         response.resolve_template(template=['foo'])
         response.connection.get.assert_called_with('foo')
 
-    def test_it_raises_404_if_no_body(self, response):
+    @mock.patch.object(logger, 'error')
+    def test_it_logs_error_and_raises_404_if_no_body(self, log_error, response):
         response.connection.get.return_value = None
         with pytest.raises(Http404):
             response.resolve_template([''])
+
+        assert log_error.called
 
     def test_it_checks_for_another_template_on_current_version(self, response):
         response.resolve_template(['app:current'])
@@ -28,3 +33,13 @@ class TestRedisTemplateResponse:
     def test_it_sets_connection_on_init(self, response):
         assert not hasattr(RedisTemplateResponse, 'connection')
         assert response.connection
+
+    @mock.patch.object(redis.StrictRedis, 'from_url')
+    def test_it_pings_connection_on_init(self, connection_init):
+        connection = mock.Mock()
+        connection_init.return_value = connection
+
+        response = self.response()
+
+        assert connection_init.called
+        assert connection.ping.called
